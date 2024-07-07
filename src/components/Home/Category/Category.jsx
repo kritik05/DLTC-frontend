a35f0e1db8465ad21efbee2b70c096accfa9f352
kritik16 <aggarwalkritik@gmail.com>
2024-07-07 14:38:46 +0530

    first commit

---
+++ src/components/Home/Category/Category.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import "./Category.scss";
+
+const Category = ({ categories ,headingText}) => {
+    const navigate = useNavigate();
+    return (
+        <div className="shop-by-category">
+            <div className="sec-heading">{headingText}</div>
+            <div className="categories">
+                { categories?.data?.map((item) => (
+                    <div
+                        key={item.id}
+                        className="category"
+                        onClick={() => {
+                            navigate(`/category/${item.id}`)
+                            window.scrollTo({
+                                top: 0,
+                                left: 0,
+                                behavior: "smooth",
+                            })
+                        }
+                    }
+                    >
+                        <div className="category-img">
+                        <img
+                            src={
+                                item.attributes.img.data.attributes.url
+                            }
+                        />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Category;
